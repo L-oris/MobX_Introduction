cf4e326343032966aaf6c723f46467feda082077
Loris <loris4526@gmail.com>
2017-11-26 15:21:15 +0100

    Move 'Temper' to be main root React component (just to make code cleaner). Create MobX array of observables, and render it inside 'Temps'

--- src/index.js
+++ src/index.js
@@ -1,8 +1,63 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {observable, computed} from 'mobx'
+import {observer} from 'mobx-react'
+
+
+class Temperature {
+
+  id = Math.random()
+  @observable unit = 'C'
+  @observable temperatureCelsius = 25
+
+  @computed get temperatureFahrenheit(){
+    return this.temperatureCelsius * (9/5) + 32
+  }
+
+  @computed get temperatureKelvin(){
+    return this.temperatureCelsius + 273.15
+  }
+
+  @computed get temperature(){
+    switch(this.unit){
+      case "C": return this.temperatureCelsius
+      case "F": return this.temperatureFahrenheit
+      case "K": return this.temperatureKelvin
+    }
+  }
+
+}
+
+const temps = observable([])
+
+//add new temperatures to observable array
+temps.push(new Temperature())
+temps.push(new Temperature())
+
+
+//attach 'temps' to browser window to manually change & debug it
+window.temps = temps
+
+
+const Temper = observer(({temperatures}) => (
+
+  <div>
+    {temperatures.map(t =>
+      <div key={t.id}>
+        {t.temperature}
+      </div>
+    )}
+  </div>
+
+))
+
+
+ReactDOM.render(
+  <Temper temperatures={temps} />,
+  document.getElementById('root'))
+
 registerServiceWorker()
